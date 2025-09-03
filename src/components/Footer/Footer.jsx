d407fb3624df064c223c8a68b87424661264565b
sky <harutazatyan45@gmail.com>
2025-09-03 11:20:42 +0400

    asdddդsաa

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,13 +8,24 @@ export default function Footer() {
         <div className="footer-content">
           <div className="footer-section">
             <h3>Contact us</h3>
-            <p>+374-033-87-88-77</p>
-            <p>hello@logoipsum.com</p>
+            <p>
+              <a href="tel:+37433878877">+374-033-87-88-77</a>
+            </p>
+            <p>
+              <a
+                href="https://wa.me/37433878877"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                WhatsApp: +374-033-87-88-77
+              </a>
+            </p>
           </div>
+
           <div className="footer-section">
             <h3>Location</h3>
-            <p>Եզնիկ Կողբացի 69 </p>
-            <p>14:00 - 03:00</p>
+            <p>Եզնիկ Կողբացի 69</p>
+            <p>15:00 - 04:00</p>
           </div>
         </div>
       </div>
